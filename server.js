36d836aaee51e4f30c073a6c03551b06d9a74ac2
pisit_boonklod <pisit.mind@gmail.com>
2019-02-02 14:09:54 +0700

    gh

--- server.js
+++ server.js
@@ -6,17 +6,30 @@ const config = {
   channelAccessToken: 'VsPqP4qFJwCTa8yjfMlSqmYzbhWMn0W9weOu7Tc9g5Ci7sO/Hit9wugxeroZySWMXHH9mYQfVNFDGPRnMeZcOcf+POw+HYWaCEd1O9fUVg5/Lbf0TSmwEyUfMUNvAjjxVwaOpOSWs6FB2AHZ0mUk9wdB04t89/1O/w1cDnyilFU=',
   channelSecret: '817d67a18ad5aa9196064cdb0bdf47bc'
 }
+const client = new Client(config);
 
-const client =new Client(config);
+
 
 app.get('/', function (req, res) {
-    res.send('Hello World!!')
+  res.send('Hello World!!')
+  
 })
+
+
 
 app.post('/webhook', middleware(config), (req, res) => {
   // req.body.events // webhook event objects
   // req.body.destination // user ID of the bot (optional)
   res.send("Hi Dude!!")
+  const event = req.body.events[0];
+  if (event.type === 'message') {
+    const message = event.message;
+
+    client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: message.type,
+    })
+  }
 })
 
 app.set('port', (process.env.PORT || 4000))
