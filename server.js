dabf0e3217d05664be5051be1eafbc34d76106bb
pisit_boonklod <pisit.mind@gmail.com>
2019-02-02 15:17:52 +0700

    double

--- server.js
+++ server.js
@@ -29,42 +29,90 @@ app.post('/webhook', middleware(config), (req, res) => {
     //   type: 'text',
     //   text: message.type,
     // })
-    client.replyMessage(event.replyToken, 
-      {
+    // client.replyMessage(event.replyToken, 
+    //   {
+    //     "type": "template",
+    //     "altText": "This is a buttons template",
+    //     "template": {
+    //         "type": "buttons",
+    //         "thumbnailImageUrl": "https://pbs.twimg.com/media/Dh6ZQvGVMAE7LFF.jpg",
+    //         "imageAspectRatio": "rectangle",
+    //         "imageSize": "cover",
+    //         "imageBackgroundColor": "#09F1F9",
+    //         "title": "Mind",
+    //         "text": "Pisit Boonklod",
+    //         "defaultAction": {
+    //             "type": "uri",
+    //             "label": "View detail",
+    //             "uri": "http://google.com/"
+    //         },
+    //         "actions": [
+    //             {
+    //               "type": "uri",
+    //               "label": "Facebook",
+    //               "uri": "https://www.facebook.com/pisit.boonklod"
+    //             },
+    //             {
+    //               "type": "message",
+    //               "label": "Add to cart",
+    //               "text": "ใส่ไปเรื่อยเดี๋ยวตีเลย"
+    //             },
+    //             {
+    //               "type": "uri",
+    //               "label": "CPE",
+    //               "uri": "http://cpe.eng.cmu.ac.th/2013"
+    //             }
+    //         ]
+    //     }
+      // })
+      client.replyMessage(event.replyToken, {
         "type": "template",
-        "altText": "This is a buttons template",
+        "altText": "this is a carousel template",
         "template": {
-            "type": "buttons",
-            "thumbnailImageUrl": "https://pbs.twimg.com/media/Dh6ZQvGVMAE7LFF.jpg",
-            "imageAspectRatio": "rectangle",
-            "imageSize": "cover",
-            "imageBackgroundColor": "#09F1F9",
-            "title": "Mind",
-            "text": "Pisit Boonklod",
-            "defaultAction": {
-                "type": "uri",
-                "label": "View detail",
-                "uri": "http://google.com/"
-            },
-            "actions": [
+            "type": "carousel",
+            "columns": [
                 {
-                  "type": "uri",
-                  "label": "Facebook",
-                  "uri": "https://www.facebook.com/pisit.boonklod"
+                  "thumbnailImageUrl": "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png/revision/latest?cb=20150808131630",
+                  "imageBackgroundColor": "#FFFFFF",
+                  "title": "this is menu",
+                  "text": "description",
+                  "actions": [
+                      {  
+                          "type":"cameraRoll",
+                          "label":"Camera roll"
+                      },
+                      {  
+                        "type":"location",
+                        "label":"Location"
+                     }
+                  ]
                 },
                 {
-                  "type": "message",
-                  "label": "Add to cart",
-                  "text": "ใส่ไปเรื่อยเดี๋ยวตีเลย"
-                },
-                {
-                  "type": "uri",
-                  "label": "CPE",
-                  "uri": "http://cpe.eng.cmu.ac.th/2013"
+                  "thumbnailImageUrl": "https://c.76.my/Malaysia/line-brown-bear-cute-pencil-case-ubiyo-1802-02-Ubiyo@6.jpg",
+                  "imageBackgroundColor": "#000000",
+                  "title": "this is menu",
+                  "text": "description",
+                  "actions": [
+                    {
+                      "type":"datetimepicker",
+                      "label":"Select date",
+                      "data":"storeId=12345",
+                      "mode":"datetime",
+                      "initial":"2017-12-25t00:00",
+                      "max":"2018-01-24t23:59",
+                      "min":"2017-12-25t00:00"
+                    },
+                    {  
+                      "type":"camera",
+                      "label":"Camera"
+                   }
+                ]
                 }
-            ]
+            ],
+            "imageAspectRatio": "rectangle",
+            "imageSize": "cover"
         }
-      })
+    })
   }
 })
 
