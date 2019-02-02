cda04fa5efe1a3082d7c256e6b70a0a79b9ada2d
pisit_boonklod <pisit.mind@gmail.com>
2019-02-02 14:39:30 +0700

    test

--- server.js
+++ server.js
@@ -29,13 +29,45 @@ app.post('/webhook', middleware(config), (req, res) => {
     //   type: 'text',
     //   text: message.type,
     // })
-    client.replyMessage(event.replyToken, {
-      type: 'sticker',
-      stickerId: '180',
-      packageId: '3'
-    })
+    client.replyMessage(event.replyToken, 
+      {
+        "type": "template",
+        "altText": "This is a buttons template",
+        "template": {
+            "type": "buttons",
+            "thumbnailImageUrl": "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png/revision/latest?cb=20150808131630",
+            "imageAspectRatio": "rectangle",
+            "imageSize": "cover",
+            "imageBackgroundColor": "#FFFFFF",
+            "title": "Menu",
+            "text": "Please select",
+            "defaultAction": {
+                "type": "uri",
+                "label": "View detail",
+                "uri": "http://google.com/"
+            },
+            "actions": [
+                {
+                  "type": "postback",
+                  "label": "Buy",
+                  "data": "action=buy&itemid=123"
+                },
+                {
+                  "type": "message",
+                  "label": "Add to cart",
+                  "text": "no no no"
+                },
+                {
+                  "type": "uri",
+                  "label": "View detail",
+                  "uri": "http://google.com"
+                }
+            ]
+        }
+      })
   }
 })
+
 
 app.set('port', (process.env.PORT || 4000))
 
