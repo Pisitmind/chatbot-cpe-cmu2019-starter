8fd743f86a7f17adbca117a170a322c0ce49570b
pisit_boonklod <pisit.mind@gmail.com>
2019-02-02 11:40:24 +0700

    comit

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
 app.post('/webhook', middleware(config), (req, res) => {
   // req.body.events // webhook event objects
   // req.body.destination // user ID of the bot (optional)
-  console.log("Webhook success")
+  res.send("Hi Dude!!")
 })
 
 app.set('port', (process.env.PORT || 4000))
